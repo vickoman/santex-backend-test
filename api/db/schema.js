8b8ac64b234f210dde675d601bad3f10dda34491
vickoman <vicmandlagasca@gmail.com>
2022-06-18 02:03:49 -0500

    main schema file

--- api/db/schema.js
+++ api/db/schema.js
@@ -5,13 +5,35 @@ const typeDefs = gql`
     type Token {
         token: String 
     }
+
     input AuthInput {
         email: String!
         password: String!
     }
 
+    input UserInput {
+        name: String
+        lastName: String
+        email: String
+        password: String
+    }
+    type User {
+        id: ID
+        name: String
+        lastName: String
+        email: String
+        created_at: String
+    }
+
+    type Query {
+        getUsers: [User]
+        getMe: User
+    }
     # Mutations
     type Mutation {
+        # Users
+        createUser(input: UserInput!) : User
+
         #Auth
         auth(input: AuthInput!) : Token
     }
