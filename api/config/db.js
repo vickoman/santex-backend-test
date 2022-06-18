8d2915c96dc3e417cb7942e60c26c86ec34c8280
vickoman <vicmandlagasca@gmail.com>
2022-06-18 02:03:34 -0500

    Database config

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_DB, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
